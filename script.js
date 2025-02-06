6af9081413a7d3cebfaa3945639328b8a9069fc4
Sarvan Thakor <97446457+sarvanthakor01@users.noreply.github.com>
2025-02-06 12:09:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function firstWord(s) {
   // your code here
-	let words = s.trim();
+	let word = s.trim();
 	words.split(" ");
 
-	return words[0];
+	return word[0];
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
